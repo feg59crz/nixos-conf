b1267bad5dd04ccb4be65d73e4411c588b133712
Fernando Cruz <fernandogomescruz@gmail.com>
2023-03-10 00:25:48 -0300

    nixos

--- flake.nix
+++ flake.nix
@@ -12,7 +12,23 @@
       virt = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
-          ./virt/configuration.nix
+          ./system/virt/configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.eee = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+        ];
+      };
+
+      acer = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./system/acer/configuration.nix
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
---
+++ system/acer/configuration.nix
@@ -0,0 +1,137 @@
+{ config, pkgs, ... }:
+
+# a515-54g-53gp
+
+let
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec "$@"
+  '';
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Sao_Paulo";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.utf8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "pt_BR.utf8";
+    LC_IDENTIFICATION = "pt_BR.utf8";
+    LC_MEASUREMENT = "pt_BR.utf8";
+    LC_MONETARY = "pt_BR.utf8";
+    LC_NAME = "pt_BR.utf8";
+    LC_NUMERIC = "pt_BR.utf8";
+    LC_PAPER = "pt_BR.utf8";
+    LC_TELEPHONE = "pt_BR.utf8";
+    LC_TIME = "pt_BR.utf8";
+  };
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the Cinnamon Desktop Environment.
+  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.desktopManager.cinnamon.enable = true;
+
+  # Configure keymap in X11
+  services.xserver = {
+    layout = "br";
+    xkbVariant = "";
+  };
+
+  # Configure console keymap
+  console.keyMap = "br-abnt2";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.eee = {
+    isNormalUser = true;
+    description = "eee";
+    extraGroups = [ "networkmanager" "docker" "wheel" "libvirtd" ];
+    packages = with pkgs; [
+      firefox
+    ];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    virt-manager
+    nvidia-offload
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Docker
+  virtualisation.docker.enable = true;
+  
+  # Libvirtd
+  virtualisation.libvirtd.enable = true;
+  programs.dconf.enable = true;
+ 
+  # intel/nvidia (optimus) & opengl
+  services.xserver.videoDrivers = [ "intel" "nvidia" ]; 
+  hardware.opengl.enable = true;
+  hardware.nvidia.prime = {
+    offload.enable = true;
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:2:0:0";
+  };
+
+  system.stateVersion = "22.11"; # Did you read the comment?
+}
