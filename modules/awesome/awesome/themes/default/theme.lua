66eddb2da2e86b7cb633803f24eb35a4ddb755a7
Fernando Gomes <fernandogomescruz@gmail.com>
2023-03-12 11:00:34 -0300

    gaps

--- modules/awesome/awesome/themes/default/theme.lua
+++ modules/awesome/awesome/themes/default/theme.lua
@@ -11,9 +11,9 @@ local themes_path = gfs.get_themes_dir()
 
 local theme = {}
 
-local mywallpaper = os.getenv("HOME")
+local mywallpaper = os.getenv("DOT_FILES")
 
-mywallpaper = mywallpaper .. "/.dotfiles/wallpaper.jpg"
+mywallpaper = mywallpaper .. "/wallpaper.jpg"
 
 theme.font          = "sans 8"
 
@@ -28,7 +28,7 @@ theme.fg_focus      = "#ffffff"
 theme.fg_urgent     = "#ffffff"
 theme.fg_minimize   = "#ffffff"
 
-theme.useless_gap   = dpi(0)
+theme.useless_gap   = dpi(20)
 theme.border_width  = dpi(1)
 theme.border_normal = "#000000"
 theme.border_focus  = "#535d6c"
