cdebe3b37676cfe37084a6b0614da494b989f704
Fernando Gomes <fernandogomescruz@gmail.com>
2023-03-12 02:29:29 -0300

    lol

--- modules/awesome/awesome/rc.lua
+++ modules/awesome/awesome/rc.lua
@@ -14,11 +14,11 @@ local beautiful = require("beautiful")
 local naughty = require("naughty")
 local menubar = require("menubar")
 local hotkeys_popup = require("awful.hotkeys_popup")
+local lain = require("modules.lain")
+local freedesktop = require("modules.freedesktop")
 -- Enable hotkeys help widget for VIM and other apps
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
-
-
 
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
