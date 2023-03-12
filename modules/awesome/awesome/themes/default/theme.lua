4e59d5b27d2a9307b780d121a3b577690555037b
Fernando Gomes <fernandogomescruz@gmail.com>
2023-03-12 03:38:49 -0300

    test

--- modules/awesome/awesome/themes/default/theme.lua
+++ modules/awesome/awesome/themes/default/theme.lua
@@ -11,6 +11,10 @@ local themes_path = gfs.get_themes_dir()
 
 local theme = {}
 
+local mywallpaper = os.getenv("HOME")
+
+mywallpaper = mywallpaper .. "/.dotfiles/wallpaper.jpg"
+
 theme.font          = "sans 8"
 
 theme.bg_normal     = "#222222"
@@ -97,7 +101,7 @@ theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar
 theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
 
-theme.wallpaper = themes_path.."default/background.png"
+theme.wallpaper = mywallpaper
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
